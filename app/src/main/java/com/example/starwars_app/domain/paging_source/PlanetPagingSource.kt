91415a04a6f1435c640ebc200cf475d925954792
EvgenieBespalov <evg.bespaloff@yandex.ru>
2023-08-09 03:55:46 +0700

    Добавил датасорс для пагинации

---
+++ app/src/main/java/com/example/starwars_app/domain/paging_source/PlanetPagingSource.kt
@@ -0,0 +1,48 @@
+package com.example.starwars_app.domain.paging_source
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.starwars_app.data.api.PlanetApi
+import com.example.starwars_app.data.converter.ConverterPlanet
+import com.example.starwars_app.data.model.PlanetApiModel
+import com.example.starwars_app.data.repository.PlanetRepositoryImpl
+import com.example.starwars_app.domain.entity.PlanetEntity
+import retrofit2.HttpException
+import java.io.IOException
+
+class PlanetPagingSource(
+    private val planetApi: PlanetApi,
+    private val converter: ConverterPlanet,
+    private val name: String
+    ): PagingSource<Int, PlanetEntity>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, PlanetEntity> {
+//        if (query.isBlank()) {
+//            return LoadResult.Page(emptyList(), prevKey = null, nextKey = null)
+//        }
+
+        try {
+            val pageNumber = params.key ?: 1
+            val response = planetApi.searchPlanet(name, pageNumber.toString())
+
+            if (response.isSuccessful) {
+                val planets = response.body()!!.results.map { converter.convertModelInEntity(it) }
+                val nextPageNumber = if (planets.isEmpty()) null else pageNumber + 1
+                val prevPageNumber = if (pageNumber > 1) pageNumber - 1 else null
+                return LoadResult.Page(planets, prevPageNumber, nextPageNumber)
+            } else {
+                return LoadResult.Error(HttpException(response))
+            }
+        } catch (e: HttpException) {
+            return LoadResult.Error(e)
+        } catch (e: Exception) {
+            return LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, PlanetEntity>): Int? {
+        val anchorPosition = state.anchorPosition ?: return null
+        val anchorPage = state.closestPageToPosition(anchorPosition) ?: return null
+        return anchorPage.prevKey?.plus(1) ?: anchorPage.nextKey?.minus(1)
+    }
+}
