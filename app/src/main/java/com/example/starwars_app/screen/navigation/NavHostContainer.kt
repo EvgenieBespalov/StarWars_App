bd68ef0ac486fe3949d95838ae3684008b96da0d
EvgenieBespalov <evg.bespaloff@yandex.ru>
2023-08-09 16:56:45 +0700

    Добавил отступ

--- app/src/main/java/com/example/starwars_app/screen/navigation/NavHostContainer.kt
+++ app/src/main/java/com/example/starwars_app/screen/navigation/NavHostContainer.kt
@@ -1,11 +1,16 @@
 package com.example.starwars_app.screen.navigation
 
+import android.service.controls.ControlsProviderService
+import android.util.Log
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.starwars_app.screen.*
+import retrofit2.HttpException
 
 @Composable
 fun NavHostContainer(
@@ -16,7 +21,7 @@ fun NavHostContainer(
     NavHost(
         navController = navController,
         startDestination = Routes.ResourcesScreenRoute.route,
-        //modifier = Modifier.padding(paddingValues = padding),
+        modifier = Modifier.padding(paddingValues = padding),
         builder = {
 
             composable(Routes.ResourcesScreenRoute.route) {
